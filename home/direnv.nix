2ebfdc10aff9d9eb4a5e5a4e41fcc199a9697f13
Charlie Meyer <charlie@charliemeyer.xyz>
2026-04-24 17:27:16 -0400

    chore: add TODO comments for direnv doCheck workaround
    Track tech debt per PR review: the direnv test suite fails against
    the current nixpkgs-unstable revision, so we override with
    doCheck=false. These TODO comments flag the workaround for removal
    once upstream passes tests again.

--- home/direnv.nix
+++ home/direnv.nix
@@ -2,6 +2,7 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    # TODO: remove doCheck override once upstream direnv tests pass in nixpkgs.
     package = pkgs.direnv.overrideAttrs (_: {doCheck = false;});
   };
 }
